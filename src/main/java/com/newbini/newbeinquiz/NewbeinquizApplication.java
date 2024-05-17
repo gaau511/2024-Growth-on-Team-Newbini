cd94875355890033dc1a34c2627c2959624a849e
gaau511 <gaau511@gmail.com>
2024-05-17 17:31:41 +0900

    initial setting & prototype

---
+++ src/main/java/com/newbini/newbeinquiz/NewbeinquizApplication.java
@@ -0,0 +1,13 @@
+package com.newbini.newbeinquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbeinquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbeinquizApplication.class, args);
+	}
+
+}
