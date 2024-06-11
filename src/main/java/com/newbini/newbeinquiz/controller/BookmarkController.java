c1b5872369f3d7fe8047866f9c46612da194e9eb
gaau511 <gaau511@gmail.com>
2024-06-12 02:01:43 +0900

    bookmark 작동 방식 변경

--- src/main/java/com/newbini/newbeinquiz/controller/BookmarkController.java
+++ src/main/java/com/newbini/newbeinquiz/controller/BookmarkController.java
@@ -1,9 +1,12 @@
-package com.newbini.newbeinquiz.member;
+package com.newbini.newbeinquiz.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Service
@@ -13,5 +16,11 @@ public class BookmarkController {
     @GetMapping("/bookmark")
     public String bookmarkPage() {
         return "bookmark";
+    }
+
+    @PostMapping("/bookmark")
+    @ResponseBody
+    public Integer bookmark(@RequestParam("index") Integer index) {
+        return index;
     }
 }
--- src/main/java/com/newbini/newbeinquiz/controller/ResultController.java
+++ src/main/java/com/newbini/newbeinquiz/controller/ResultController.java
@@ -1,15 +1,47 @@
 package com.newbini.newbeinquiz.controller;
 
+import com.newbini.newbeinquiz.dto.request.QuizForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/result")
 public class ResultController {
 
+
+// TODO 얘 원상복구 시키기
+//    @GetMapping
+//    public String showResult() {
+//        return "result";
+//    }
+
     @GetMapping
-    public String showResult() {
+    public String tempShowResult(Model model) {
+        QuizForm quiz = makeMockQuiz();
+        model.addAttribute("quiz", quiz);
         return "result";
+    }
+
+    private QuizForm makeMockQuiz() {
+        QuizForm quiz = new QuizForm();
+        List<String> tempOptionList = new ArrayList<>();
+        tempOptionList.add("정답");
+        tempOptionList.add("오답");
+        QuizForm.Question question1 = new QuizForm.Question("객관식","첫번째 객관식 질문", "정답", tempOptionList);
+        QuizForm.Question question2 = new QuizForm.Question("O/X","두번째 OX 질문", "O", null);
+        QuizForm.Question question3 = new QuizForm.Question("주관식","세번째 주관식 질문", "그럴수도 아닐수도 있다.", null);
+        List<QuizForm.Question> questions = new ArrayList<>();
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        quiz.setQuestions(questions);
+
+        return quiz;
     }
 }
