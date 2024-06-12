fce2886916aac89f5e184487e061198d38f6922b
gaau511 <gaau511@gmail.com>
2024-06-13 02:01:11 +0900

    login 검증을 위한 interceptor 구현 및 등록, login 상태에 따라 nav-bar 항목 변화

---
+++ src/main/java/com/newbini/newbeinquiz/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.newbini.newbeinquiz.config;
+
+import com.newbini.newbeinquiz.Interceptor.LoginMemberInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginMemberInterceptor loginMemberInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginMemberInterceptor)
+                .addPathPatterns("/", "/bookmark", "/parameter", "/result", "/upload"); // 특정 경로에만 인터셉터를 적용합니다.
+    }
+}
+
