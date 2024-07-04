507a77a4a07f1bef88f85860eb338a46750ad2de
gaau511 <gaau511@gmail.com>
2024-07-04 19:10:00 +0900

    Member Entity Mapping

--- src/main/java/com/newbini/newbeinquiz/member/Member.java
+++ src/main/java/com/newbini/newbeinquiz/member/Member.java
@@ -1,23 +1,31 @@
 package com.newbini.newbeinquiz.member;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import java.util.UUID;
 
 @Data
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "login_id", length = 20)
+    private String loginId;
+
+    @Column(name = "password", length = 20)
+    private String password;
+
+    @Column(name = "name", length = 20)
+    private String name;
+
     public Member() {
-        this.id = UUID.randomUUID().toString();
     }
 
     public Member(String loginId, String password, String name) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
-        this.id = UUID.randomUUID().toString();
     }
-
-    private String id;
-    private String loginId;
-    private String password;
-    private String name;
 }
