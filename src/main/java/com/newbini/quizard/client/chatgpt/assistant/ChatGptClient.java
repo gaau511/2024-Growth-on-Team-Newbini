f7f6ff46c0fb94bb2f27e2a0d28f85a6aa61ca3c
gaau511 <gaau511@gmail.com>
2025-01-08 15:01:47 +0900

    refactor(ChatGptClient): ChatGpt API 요청 클래스를 'client.chatgpt'로 분리
    코드의 가독성과 구조적 일관성을 위해 assistant / message /
    thread 로 분리된 ChatGpt API 요청 클래스를 하나의 클래스에서 관리.
    현재는 Assistant를 생성하는 메서드만 migration함.

---
+++ src/main/java/com/newbini/quizard/client/chatgpt/assistant/ChatGptClient.java
@@ -0,0 +1,47 @@
+package com.newbini.quizard.client.chatgpt.assistant;
+
+import com.newbini.quizard.client.chatgpt.common.OpenAIBasicHeaderConst;
+import com.newbini.quizard.dto.request.ChatGptAssistantResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.newbini.quizard.client.chatgpt.common.InstructionConst.*;
+
+@Component
+@RequiredArgsConstructor
+public class ChatGptClient {
+
+    @Value("${openai.api.key}")
+    private String openAiApiKey;
+
+    private final RestTemplate restTemplate;
+
+    public ChatGptAssistantResponse createAssistant() {
+        HttpHeaders header = OpenAIBasicHeaderConst.getBasicHeader(openAiApiKey);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("name", NAME);
+        body.put("model", MODEL);
+        body.put("tools", TOOLS);
+        body.put("temperature", TEMPERATURE);
+        body.put("instructions", INSTRUCTIONS);
+
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(body, header);
+
+        return restTemplate.exchange(
+                ASSISTANT_BASE_URL,
+                HttpMethod.POST,
+                requestEntity,
+                ChatGptAssistantResponse.class
+        ).getBody();
+
+    }
+}
