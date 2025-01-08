db51a298f4c9b9c29a56ad349e2c50c561cbf11a
gaau511 <gaau511@gmail.com>
2025-01-08 23:07:05 +0900

    remove(AppConfig): Bean 등록 최적화

--- src/main/java/com/newbini/quizard/config/AppConfig.java
+++ src/main/java/com/newbini/quizard/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.newbini.quizard.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.newbini.quizard.client.chatgpt.assistant.AssistantGenerator;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,28 +9,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
-    @Value("${openai.api.key}")
-    private String key;
-
-    @Bean(name = "openAiApiKey")
-    public String getApiKey() {
-        return key;
-    }
-
-    @Bean
-    public AssistantGenerator assistantGenerator(RestTemplate restTemplate) {
-        return new AssistantGenerator(key, restTemplate());
-    }
-
-    /**
-     * Open Ai Audio Api Bean 등록
-     */
-    @Bean
-    public OpenAiAudioApi openAiAudioApi() {
-        return new OpenAiAudioApi(key);
-    }
-
     @Bean
     public RestTemplate restTemplate() {return new RestTemplate();}
 
